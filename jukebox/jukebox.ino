eb38d186812844d040c870953ef94d1aab30be39
Camila Guzman <camila.guzman023@gmail.com>
2026-05-31 17:40:45 -0400

    setup buttons and solved debounce issue

--- jukebox/jukebox.ino
+++ jukebox/jukebox.ino
@@ -1,7 +1,36 @@
 #include <TFT_eSPI.h>
 
-TFT_eSPI tft = TFT_eSPI(320, 240);
+#define YELLOW_BTN 25
+#define WHITE_BTN 26
+#define RED_BTN 27
+#define GREEN_BTN 14
 
+unsigned int debounceDelayYellow = 50;
+unsigned long lastYellowDebounce = millis();
+byte yellowBtnState = LOW;
+
+unsigned int debounceDelayWhite = 50;
+unsigned long lastWhiteDebounce = millis();
+byte whiteBtnState = LOW;
+
+unsigned int debounceDelayRed = 50;
+unsigned long lastRedDebounce = millis();
+byte redBtnState = LOW;
+
+unsigned int debounceDelayGreen = 50;
+unsigned long lastGreenDebounce = millis();
+byte greenBtnState = LOW;
+
+TFT_eSPI tft = TFT_eSPI(320, 240);
+enum State {
+  IDLE,
+  MUSIC
+};
+
+State state = IDLE;
+State currentScreen;
+
+// BMO FUNCTIONS
 void drawSmile(int cx, int cy, int r) {
   for (int a = 20; a <= 160; a++) {
     float rad = a * DEG_TO_RAD;
@@ -13,8 +42,7 @@ void drawSmile(int cx, int cy, int r) {
   }
 }
 
-
-void BMOneutralFace() {
+void BMOidleFace() {
   int w = tft.width();
   int h = tft.height();
 
@@ -34,11 +62,116 @@ void BMOneutralFace() {
 }
 
 void setup() {
+  Serial.begin(115200);
+
+  pinMode(YELLOW_BTN, INPUT);
+  pinMode(WHITE_BTN, INPUT);
+  pinMode(RED_BTN, INPUT);
+  pinMode(GREEN_BTN, INPUT);
+
   tft.init();
   tft.setRotation(4);
 
-  BMOneutralFace();
+  BMOidleFace();
+}
+
+// BUTTON FUNCTIONS
+void handleYellowBtn() {
+  unsigned long timeNow = millis();
+
+  if(timeNow - lastYellowDebounce > debounceDelayYellow) {
+    byte newBtnState = digitalRead(YELLOW_BTN);
+
+    if(newBtnState != yellowBtnState) {
+      yellowBtnState = newBtnState;
+      lastYellowDebounce = timeNow;
+
+      if(yellowBtnState == HIGH) {
+        Serial.println("Yellow button pressed");
+        state = MUSIC;
+      }
+      else {
+        Serial.println("Yellow button released");
+      }
+    }
+  }
+}
+
+void handleWhiteBtn() {
+  unsigned long timeNow = millis();
+
+  if(timeNow - lastWhiteDebounce > debounceDelayWhite) {
+    byte newBtnState = digitalRead(WHITE_BTN);
+
+    if(newBtnState != whiteBtnState) {
+      whiteBtnState = newBtnState;
+      lastWhiteDebounce = timeNow;
+
+      if(whiteBtnState == HIGH) {
+        Serial.println("White button pressed");
+      }
+      else {
+        Serial.println("White button released");
+      }
+    }
+  }
+}
+
+void handleRedBtn() {
+  unsigned long timeNow = millis();
+
+  if(timeNow - lastRedDebounce > debounceDelayRed) {
+    byte newBtnState = digitalRead(RED_BTN);
+
+    if(newBtnState != redBtnState) {
+      redBtnState = newBtnState;
+      lastRedDebounce = timeNow;
+
+      if(redBtnState == HIGH) {
+        Serial.println("Red button pressed");
+        state = IDLE;
+      }
+      else {
+        Serial.println("Red button released");
+      }
+    }
+  }
+}
+
+void handleGreenBtn() {
+  unsigned long timeNow = millis();
+
+  if(timeNow - lastGreenDebounce > debounceDelayGreen) {
+    byte newBtnState = digitalRead(GREEN_BTN);
+
+    if(newBtnState != greenBtnState) {
+      greenBtnState = newBtnState;
+      lastGreenDebounce = timeNow;
+
+      if(greenBtnState == HIGH) {
+        Serial.println("Green button pressed");
+      }
+      else {
+        Serial.println("Green button released");
+      }
+    }
+  }
 }
 
 void loop() {
+  handleYellowBtn();
+  handleWhiteBtn();
+  handleRedBtn();
+  handleGreenBtn();
+
+  if(state != currentScreen) {
+    currentScreen = state;
+
+    if(state == IDLE) {
+      BMOidleFace();
+    }
+    else if(state == MUSIC) {
+      // draw music screen
+    }
+  }
 }
