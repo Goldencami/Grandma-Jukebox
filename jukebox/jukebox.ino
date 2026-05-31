a6bb9b161075ed7ab0ce0f6ed66c08e61bbd3998
Camila Guzman <camila.guzman023@gmail.com>
2026-05-30 21:19:47 -0400

    fixed smile width and height

--- jukebox/jukebox.ino
+++ jukebox/jukebox.ino
@@ -4,32 +4,33 @@ TFT_eSPI tft = TFT_eSPI(320, 240);
 
 void drawSmile(int cx, int cy, int r) {
   for (int a = 20; a <= 160; a++) {
-    float rad = a * 3.14159 / 180.0;
+    float rad = a * DEG_TO_RAD;
 
     int x = cx + r * cos(rad);
     int y = cy + r * sin(rad);
 
-    // thickness = key part (makes it visible as a curve)
     tft.fillCircle(x, y, 2, TFT_BLACK);
   }
 }
+
 
 void BMOneutralFace() {
   int w = tft.width();
   int h = tft.height();
 
   // background
-  tft.fillScreen(tft.color565(150, 240, 186));
+  uint16_t bmoGreen = tft.color565(150, 240, 186);
+  tft.fillScreen(bmoGreen);
 
   // eye settings
   int eyeR = 7;
   int eyeY = h * 0.35;
 
-  tft.fillCircle(w * 0.4, eyeY, eyeR, TFT_BLACK);
-  tft.fillCircle(w * 0.6, eyeY, eyeR, TFT_BLACK);
+  tft.fillCircle(w * 0.3, eyeY, eyeR, TFT_BLACK);
+  tft.fillCircle(w * 0.7, eyeY, eyeR, TFT_BLACK);
 
   // smile :)
-  drawSmile(w / 2, h * 0.55, 35);
+  drawSmile(w / 2, h * 0.43, 40);
 }
 
 void setup() {
